6a8a007c8bbfdf56d4d5147149c0f8300165f6f1
PMLujan <lujan_paredes@hotmail.com.ar>
2022-08-16 19:46:38 -0300

    Objetos

--- js/js9.js
+++ js/js9.js
@@ -1,1 +1,14 @@
 // Objetos
+let producto = {
+    nombreProducto:"Monitor de 20\"",
+    precio:"$200",
+    diponible:true,
+}
+
+console.log(producto);
+
+//acceder a las propiedades del objeto:
+
+console.log(producto.precio);//opcion mas usada
+
+console.log(producto["nombreProducto"]);
