7c480419f0aeb50e3ab4762366de9c75784af675
PMLujan <lujan_paredes@hotmail.com.ar>
2022-08-20 11:43:45 -0300

    Estructuras de control if

---
+++ js/js18.js
@@ -0,0 +1,21 @@
+// Estructuras de control
+
+const edad= 15;
+
+if(edad >= 18){
+    console.log("Es mayor de edad");
+}else{
+    console.log("Es menor de edad");
+    }
+
+//con mas condiciones
+
+const rol= "administrador";
+
+if(rol === "administrador"){
+            console.log("Acceso al sistema");
+}else if(rol === "editor"){
+            console.log("Acceso limitado");
+}else{
+            console.log("No tienes acceso");
+};
