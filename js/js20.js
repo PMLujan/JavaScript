209c6376159d31f91f969ec2a9b64aace5b4b6ea
PMLujan <lujan_paredes@hotmail.com.ar>
2022-08-23 16:01:12 -0300

    forEach & map

--- js/js20.js
+++ js/js20.js
@@ -1,17 +1,17 @@
 // iteradores
 //foor loop
 
-for(let i=0; i<10 ; i++){
-    console.log(i)
-}
+// for(let i=0; i<10 ; i++){
+//     console.log(i)
+// }
 
-for(let i=0; i<100 ; i++){
-    if(i % 2 === 0){
-        console.log(`Es par ${i}`);
-    }else{
-        console.log(`Es impar ${i}`);
-    }
-}
+// for(let i=0; i<100 ; i++){
+//     if(i % 2 === 0){
+//         console.log(`Es par ${i}`);
+//     }else{
+//         console.log(`Es impar ${i}`);
+//     }
+// }
 
 let carrito=[
     {nombre:" TV LED", precio:"200"},
@@ -20,11 +20,28 @@ let carrito=[
     {nombre: "microfono", precio:"800"}
 ]
 
-for (let i=0; i<carrito.length;i++){ //va imprimir todo el array
-    console.log(carrito[i])
-}
-for (let i=0; i<carrito.length;i++){// va imprimir solo la propiedad nombre der array
-    console.log(carrito[i].nombre)
-}
+// for (let i=0; i<carrito.length;i++){ //va imprimir todo el array
+//     console.log(carrito[i])
+// }
+// for (let i=0; i<carrito.length;i++){// va imprimir solo la propiedad nombre der array
+//     console.log(carrito[i].nombre)
+// }
 
 
+// // WHILE LOOP
+// let i=0; // indice
+// while(i<10){// condicio
+//     console.log(i)
+//     i++; //incremento
+// }
+
+// forEach
+
+carrito.forEach(function(elemento){
+    console.log(elemento);            //imprime en consola cada elemento de la matriz
+});
+
+// // MAP
+carrito.map(function(elemento){
+    console.log(elemento)
+});
